bac0a25880aeb773ad7ee30c188b0e4b4b51558d
Naman Jain <namanjain@Namans-MacBook-Air.local>
2019-12-09 12:38:30 +0530

    barrel explosion handled

--- Assignment 4/Assignment 4/Doom Final/Components/Barrel/Barrel.gd
+++ Assignment 4/Assignment 4/Doom Final/Components/Barrel/Barrel.gd
@@ -1,29 +1,52 @@
 extends StaticBody
 
+onready var anim_audio = $BarrelAudio
+
 var health = 3
+
+var IMPACT = 3
+var BURST_RADIUS = 10
+
 var expl = false
 var fire_particles
-var blast_damage
 
+var player = null
+
+#-----------------------------------------------------------
 func _ready():
   fire_particles = $Fire
   fire_particles.emitting = false
-  blast_damage = $blast_area
+  add_to_group( 'obstacles' )
 
-
-func explode( howMuch = 1) :
+#-----------------------------------------------------------
+func hurt( howMuch = 1) :
   health -= howMuch
  
   if health <= 0 and expl == false :
     expl = true
     fire_particles.emitting = true
-    print("boom")
-  #  $'../Barrel Audio'._playSound( 'boom' )
-    
-    var bodies = blast_damage.get_overlapping_bodies()
-    for body in bodies:
-        if body.has_method("hurt"):
-           body.hurt(Utils.dieRoll('1d2') )
-        elif body.has_method("explode") :
-           body.explode()
-     
+    print( '%s burst.' % name )
+
+    anim_audio._playSound( "burst" )
+    player.burstImpact( translation, BURST_RADIUS, IMPACT )
+    get_tree().call_group( 'zombies', 'burstImpact', translation, BURST_RADIUS, IMPACT )
+    yield(get_tree().create_timer(1.0), "timeout")
+    get_tree().call_group( 'obstacles', 'burstImpact', translation, BURST_RADIUS, IMPACT )
+    get_tree().call_group( 'spawns', 'burstImpact', translation, BURST_RADIUS, IMPACT )
+
+  else :
+    print( '%s wounded by %d, now has %d.' % [ name, howMuch, health ] )
+
+#-----------------------------------------------------------
+func burstImpact( burst_translation, radius = 1, impact = 1 ):
+  var dist = translation.distance_to( burst_translation ) 
+  if not expl and dist <= radius:
+    hurt( impact )
+
+#-----------------------------------------------------------
+func setHealth( hp ) :
+  health =  hp
+
+#-----------------------------------------------------------
+func set_player( p ) :
+  player = p
